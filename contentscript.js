eb0b250b2ada31866c27875cf00e748a3f8bcf5a
Qianqian Zhao <chance.zhao@gmail.com>
2012-10-14 17:11:51 -0800

    working on tag passing

--- contentscript.js
+++ contentscript.js
@@ -7,14 +7,7 @@ for (var i = 0; i < myMessage.length; i++){
 	str += myMessage[i].innerHTML;	
 }
 console.log(str);
-/* Some test code for parsing
- * //JSON.stringify(myMessage);
-var myMessageString = $.parseJSON('[' + str + ']');
-console.log("About to send...");
-console.log(myMessage);
-console.log("String form");
-console.log(myMessageString);
-*/
+
 var myMessageString = "hello";
 chrome.extension.sendRequest({greeting: str}, function(response) {
 	console.log("Sent Request");
