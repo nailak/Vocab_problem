13e4fd3e6f9f6d534545528274b288fef10f63c3
Suhani N Mehta <suhani@berkeley.edu>
2012-10-20 11:17:24 -0700

    Cleaning up and adding template structures

--- js/Tabs.js
+++ js/Tabs.js
@@ -1,57 +1,33 @@
 
 $(document).ready(function()
 	{
-	//HIDE ALL DIVS
-	//HideAllDivs();
-										 
-	//SHOW FIRST DIV BY DEFAULT
-	// $('#div0').show();
-	// $('#tag0').parent().addClass("active");
-	// console.log('THIS IS THE PARENT-12------------------------------------');
-	// console.log($('#tag1').parent());
-	
-	//CHANGE HEIGHT OF "BOX" AS PER CONTENT
-	//ChangeBoxHeight('#div0');
-
-
 	function HideAllDivs(){
 		$('#Box').children().each(function(){
 			$(this).hide();
 		});
 	}
-	
-	/*function ChangeBoxHeight(divid){
-		HeightOfDivContent=$(divid).height();
-		$('#Box').css("height",HeightOfDivContent);
-	}*/
 
 	$('#Tabs a').live('click', function() {
-	// $("#Tabs a").on('click', function(){
 		//FIRST, CHANGE ALL TABS TO NORMAL
 		$('.nav-pills li').each(function(){
 			$(this).removeClass("active");
 		});
+
 		//NEXT, CHNAGE STYLE OF SELECTED TAB
 		$(this).parent().addClass("active");
-		console.log('changing tab');
 		//SHOW RESPECTIVE DIV IN "BOX"
-		// tagname=$(this).text();  //Naila: changed this so it gets ID instad of text
 		tagname=$(this).attr('id');
 
 		//ABOVE COMMAND RETURNS CAPITALISED ID. THEREFORE NEED TO CONVERT TO LOWERCASE FOR MATCHING TEXT IN "REPLACE"
 		tagname=tagname.toLowerCase();
-		console.log(tagname);
-
+		
 		//HIDE ALL DIV
 		HideAllDivs();
 		
 		//SHOW NEW DIV									 
 		divname='#'+tagname.replace('tag','div');
-		console.log(divname);
 		$(divname).show();
-		
-		//CHANGE HEIGHT OF "BOX" AS PER CONTENT
-		//ChangeBoxHeight(divname);
+
 
 		return false;
 	});
